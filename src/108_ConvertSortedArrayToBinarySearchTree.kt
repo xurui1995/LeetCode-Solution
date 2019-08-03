72f400bba55ce64930c8c325bcd74f4c544ae737
许锐 <xur@2345.com>
2019-08-03 15:27:26 +0800

    add solution108

---
+++ src/108_ConvertSortedArrayToBinarySearchTree.kt
@@ -0,0 +1,32 @@
+import org.junit.Test
+
+class Solution108 {
+
+    @Test
+    fun test() {
+        println(Solution102().levelOrder(sortedArrayToBST(intArrayOf(-10, -3, 0, 5, 9))))
+    }
+
+    fun sortedArrayToBST(nums: IntArray): TreeNode? {
+        if (nums.isEmpty()) {
+            return null
+        }
+        return sortedArrayToBSTSTep(nums, 0, nums.lastIndex)
+    }
+
+    fun sortedArrayToBSTSTep(nums: IntArray, start: Int, end: Int): TreeNode {
+        if (start == end) {
+            return TreeNode(nums[start])
+        } else {
+            val mid = (start + end) / 2
+            val root = TreeNode(nums[mid])
+            if (start != mid) {
+                root.left = sortedArrayToBSTSTep(nums, start, mid - 1)
+            }
+            if (start != end) {
+                root.right = sortedArrayToBSTSTep(nums, mid + 1, end)
+            }
+            return root
+        }
+    }
+}
