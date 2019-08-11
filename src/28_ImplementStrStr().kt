e94acc48ff5df9a6cf2e0e88d4e8e91d8cc84c65
许锐 <xur@2345.com>
2019-08-11 21:43:58 +0800

    solution 28 add sunday solution

--- src/28_ImplementStrStr().kt
+++ src/28_ImplementStrStr().kt
@@ -50,6 +50,43 @@ class Solution28 {
         return next
     }
 
+
+    fun sunday(haystack: String, neddle: String): Int {
+        var i = 0
+        if (haystack == neddle) {
+            return 0
+        }
+        while (i <= haystack.lastIndex) {
+            if (isOk(haystack, neddle, i)) {
+                return i
+            } else {
+                i += neddle.length
+                if (i <= haystack.lastIndex) {
+                    val index = neddle.lastIndexOf(haystack[i])
+                    if (index != -1) {
+                        i -= index
+                    } else {
+                        i++
+                    }
+                }
+            }
+        }
+        return -1
+    }
+
+    fun isOk(haystack: String, neddle: String, index: Int): Boolean {
+        if (index + neddle.length - 1 > haystack.lastIndex) {
+            return false
+        }
+        for (i in index until index + neddle.length) {
+            if (haystack[i] != neddle[i - index]) {
+                return false
+            }
+        }
+        return true
+    }
+
+
     @Test
     fun test() {
         println(getNext("ABAB").toList())
@@ -57,5 +94,10 @@ class Solution28 {
         println(strStr("hello", "ll"))
         println(strStr("aaaaa", "bba"))
         println(strStr("thisisbba", "is"))
+
+
+        println(sunday("hello", "ll"))
+        println(sunday("aaaaa", "bba"))
+        println(sunday("", ""))
     }
 }
