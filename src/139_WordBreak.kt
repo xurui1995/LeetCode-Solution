34775bd950b3d3e4cb4b82fd4666b5afe4a5efca
许锐 <xur@2345.com>
2019-08-13 09:16:51 +0800

    add solution139

--- src/139_WordBreak.kt
+++ src/139_WordBreak.kt
@@ -1,32 +1,36 @@
-//package LeetCode
-//
-//class Solution139 {
-//    fun wordBreak(s: String, wordDict: List<String>): Boolean {
-//        if (wordDict.isEmpty()) {
-//            return false
-//        }
-//        return  break1(s, wordDict, wordDict.size-1)
-//    }
-//
-//    fun break1(s: String, wordDict: List<String>, n: Int): Boolean {
-//
-//        if (s.contains(wordDict[n])) {
-//            if (s == wordDict[n]) {
-//                return true
-//            }
-//            val start = s.indexOf(wordDict[n])
-//
-//            if (newS == "") {
-//                return true
-//            } else {
-//                return break1(newS, wordDict, n) || (if (n-1>=0) break1(s, wordDict, n-1) else false)
-//            }
-//        } else {
-//            if (n == 0) {
-//                return false
-//            } else {
-//                return break1(s, wordDict, n-1)
-//            }
-//        }
-//    }
-//}
+package LeetCode
+
+import org.junit.Test
+
+class Solution139 {
+
+    @Test
+    fun test() {
+        println(wordBreak("ccbb", listOf("bc", "cb")))
+        println(wordBreak("applepenapple", listOf("apple", "pen")))
+        println(wordBreak("bb", listOf("a", "b", "bbb", "bbbb")))
+    }
+
+    fun wordBreak(s: String, wordDict: List<String>): Boolean {
+        if (s.isEmpty()) {
+            return false
+        }
+        val flags = BooleanArray(s.length)
+        flags[0] = wordDict.contains(s[0].toString())
+        for (i in 1..s.lastIndex) {
+            if (wordDict.contains(s.substring(0, i + 1))) {
+                flags[i] = true
+                continue
+            }
+            for (j in i - 1 downTo 0) {
+                if (flags[j]) {
+                    if (wordDict.contains(s.substring(j + 1, i + 1))) {
+                        flags[i] = true
+                        continue
+                    }
+                }
+            }
+        }
+        return flags.last()
+    }
+}
