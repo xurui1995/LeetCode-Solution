f0290417eedb56526f8457b6a728b530582a25f3
许锐 <xur@2345.com>
2019-08-14 15:45:19 +0800

    add solution33

--- src/33_SearchInRotatedSortedArray.kt
+++ src/33_SearchInRotatedSortedArray.kt
@@ -2,43 +2,47 @@ import org.junit.Test
 
 class Solution33 {
     fun search(nums: IntArray, target: Int): Int {
-        var left = 0
-        var right = nums.size-1
-
-        while (left < right) {
-            var mid: Int = (left + right)/2
-            if (left == mid) {
-                if (nums[mid] != target) {
-                    left++
-                    mid++
-                }
-            }
-            when {
-                target == nums[mid] -> return mid
-                nums[mid] > target -> {
-                    if (nums[mid] >= nums[0]) {
-                        left = mid
-                    } else {
-                        right = mid
-                    }
-                }
-                else -> {
-                    left = mid
-                }
-            }
-        }
-
         if (nums.isEmpty()) {
             return -1
         }
-        return if (nums[left] == target) left else -1
-    }
+        var left = 0
+        var right = nums.lastIndex
+        val isTargetLeft = target > nums.last()
 
+        while (left + 1 < right) {
+            val mid = left + (right - left) / 2
+            if (nums[mid] == target) {
+                return mid
+            } else if (isTargetLeft) {
+                if (nums[mid] > target || nums[mid] <= nums.last()) {
+                    right = mid
+                } else {
+                    left = mid
+                }
+            } else {
+                if (nums[mid] < target || nums[mid] > nums.last()) {
+                    left = mid
+                } else {
+                    right = mid
+                }
+            }
+        }
+
+        if (nums[left] == target) {
+            return left
+        }
+
+        if (nums[right] == target) {
+            return right
+        }
+
+        return -1
+    }
 
 
     @Test
     fun test() {
-        val array = intArrayOf(1,3,5)
+        val array = intArrayOf(1, 3, 5)
         print(search(array, 1))
     }
 }
