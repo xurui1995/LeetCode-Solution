3b29629bda11bcfc5843dbe16ccb318b52bf9ab6
许锐 <xur@2345.com>
2019-08-25 22:35:46 +0800

    add solution168

---
+++ src/168_ExcelSheetColumnTitle.kt
@@ -0,0 +1,31 @@
+import org.junit.Test
+
+class Solution168 {
+
+    @Test
+    fun test() {
+        println(convertToTitle(1))
+        println(convertToTitle(28))
+        println(convertToTitle(701))
+        println(convertToTitle(52))
+        println(convertToTitle(703))
+
+    }
+
+
+    fun convertToTitle(n: Int): String {
+        val ans = StringBuilder()
+        var newN = n
+        while (true) {
+            if (newN <= 26) {
+                ans.append(('A' + newN - 1))
+                break
+            } else {
+                val count = (newN -1)/ 26
+                ans.append(convertToTitle(count))
+                newN -= count * 26
+            }
+        }
+        return ans.toString()
+    }
+}
