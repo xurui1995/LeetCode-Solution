068db9f4a3b3401dbe08e8cf087c46d5ca21b5db
许锐 <xur@2345.com>
2019-09-08 00:46:34 +0800

    add solution338

---
+++ src/338_CountingBits.kt
@@ -0,0 +1,22 @@
+import org.junit.Test
+
+class Solution338 {
+
+    @Test
+    fun test() {
+        println(countBits(10).toList())
+    }
+
+    fun countBits(num: Int): IntArray {
+        val array = IntArray(num + 1)
+        for (i in 1..num) {
+            if (i and 0x1 == 1) {
+                array[i] = array[i - 1] + 1
+            } else {
+                array[i] = array[i shr 1]
+            }
+        }
+
+        return array
+    }
+}
