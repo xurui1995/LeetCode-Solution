296e168a44a3300168a42e77121438d498d7f56f
许锐 <xur@2345.com>
2019-09-11 18:48:32 +0800

    add solution205

---
+++ src/205_IsomorphicStrings.kt
@@ -0,0 +1,37 @@
+import org.junit.Test
+
+class Solution205 {
+
+    @Test
+    fun test() {
+        println(isIsomorphic("egg", "add"))
+        println(isIsomorphic("foo", "bar"))
+        println(isIsomorphic("paper", "title"))
+        println(isIsomorphic("ab", "aa"))
+    }
+
+    fun isIsomorphic(s: String, t: String): Boolean {
+        if (s.isEmpty()) {
+            return true
+        }
+        val map = mutableMapOf<Char, Char>()
+        val mapValue = mutableSetOf<Char>()
+        for (i in s.indices) {
+
+            if (map.contains(s[i])) {
+
+                if (t[i] != map[s[i]]) {
+                    return false
+                }
+
+            } else {
+                if (mapValue.contains(t[i])) {
+                    return false
+                }
+                map[s[i]] = t[i]
+                mapValue.add(t[i])
+            }
+        }
+        return true
+    }
+}
