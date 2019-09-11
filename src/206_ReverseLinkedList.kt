47d9fc643c35f581876cf46ebca82ac51871df19
许锐 <xur@2345.com>
2019-09-11 20:32:26 +0800

    add solution206

---
+++ src/206_ReverseLinkedList.kt
@@ -0,0 +1,33 @@
+import org.junit.Test
+
+class Solution206 {
+
+    @Test
+    fun test() {
+        val node1 = ListNode(1)
+        val node2 = ListNode(2)
+        val node3 = ListNode(3)
+        val node4 = ListNode(4)
+        val node5 = ListNode(5)
+        node1.next = node2
+        node2.next = node3
+        node3.next = node4
+        node4.next = node5
+
+        reverseList(node1)?.print()
+    }
+
+    fun reverseList(head: ListNode?): ListNode? {
+        if (head == null) {
+            return null
+        }
+        if (head.next == null) {
+            return head
+        }
+        val newHead = reverseList(head.next)
+        head.next?.next = head
+        head.next = null
+        return newHead
+    }
+
+}
