e13e99c307fd5247808eae80c3ab5d39717a6197
许锐 <xur@2345.com>
2019-09-19 14:05:06 +0800

    add solution216

---
+++ src/216_CombinationSumIII.kt
@@ -0,0 +1,39 @@
+import org.junit.Test
+
+class Solution216 {
+
+
+    @Test
+    fun test() {
+        println(combinationSum3(2, 1))
+    }
+
+    fun combinationSum3(k: Int, n: Int): List<List<Int>> {
+        if (n < 1 || n > 50) {
+            return emptyList()
+        }
+        val ans = mutableListOf<List<Int>>()
+        helper(1, k, n, mutableListOf(), ans)
+        return ans
+    }
+
+    fun helper(start: Int, k: Int, n: Int, temp: MutableList<Int>, ans: MutableList<List<Int>>) {
+
+        if (k == 1) {
+            if (n in start..9) {
+                temp.add(n)
+                ans.add(temp.toList())
+                temp.removeAt(temp.lastIndex)
+            }
+        } else {
+            for (i in start..9) {
+                if (start > n) {
+                    return
+                }
+                temp.add(i)
+                helper(i + 1, k - 1, n - i, temp, ans)
+                temp.removeAt(temp.lastIndex)
+            }
+        }
+    }
+}
