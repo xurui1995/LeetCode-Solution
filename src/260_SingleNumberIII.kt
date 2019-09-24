906dd79401b49559507df359a9a3ed318d0ef618
许锐 <xur@2345.com>
2019-09-24 14:31:01 +0800

    add solution260

---
+++ src/260_SingleNumberIII.kt
@@ -0,0 +1,34 @@
+import org.junit.Test
+
+class Solution260 {
+
+    @Test
+    fun test() {
+        println(singleNumber(intArrayOf(1,2,1,3,2,5)).toList())
+
+    }
+
+    fun singleNumber(nums: IntArray): IntArray {
+        var a = 0
+        nums.forEach {
+            a = a xor it
+        }
+        for (i in 0..32) {
+            if ((a ushr i) and 0x1 != 0) {
+                a = i
+                break
+            }
+        }
+
+        var ans1 = 0
+        var ans2 = 0
+        nums.forEach {
+            if ((it ushr a) and 0x1 != 0) {
+                ans1 = ans1 xor it
+            } else {
+                ans2 = ans2 xor it
+            }
+        }
+        return intArrayOf(ans1, ans2)
+    }
+}
