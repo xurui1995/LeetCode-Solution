d09337a4acd3c059250256c2571e5a905d6dda36
许锐 <xur@2345.com>
2019-09-26 16:16:08 +0800

    add solution235

---
+++ src/_235_LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,21 @@
+class Solution235 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int min = p.val < q.val?p.val:q.val;
+        int max = p.val > q.val?p.val:q.val;
+        if (root.val > max) {
+            return lowestCommonAncestor(root.left, p,q);
+        }
+        if (root.val < min) {
+            return lowestCommonAncestor(root.right, p,q);
+        }
+        return root;
+    }
+}
