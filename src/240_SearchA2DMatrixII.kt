76af9a7d5c380f4809c3ac4e977f51b53c94ec1f
许锐 <xur@2345.com>
2019-09-26 17:37:59 +0800

    add solution240

---
+++ src/240_SearchA2DMatrixII.kt
@@ -0,0 +1,18 @@
+class Solution240 {
+
+    fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+        if (matrix.isNotEmpty()) {
+            var i = 0
+            var j = matrix[0].lastIndex
+
+            while (i <= matrix.lastIndex && j >= 0) {
+                when {
+                    target == matrix[i][j] -> return true
+                    target > matrix[i][j] -> i++
+                    else -> j--
+                }
+            }
+        }
+        return false
+    }
+}
