b9ac5a8123cc25d24e04f3d0b3574b7d5bde2e1b
许锐 <xur@2345.com>
2019-09-26 11:53:07 +0800

    add solution289

---
+++ src/289_GameOfLife.kt
@@ -0,0 +1,71 @@
+import org.junit.Test
+
+class Solution289 {
+
+    @Test
+    fun test() {
+        val array = arrayOf(
+                intArrayOf(0,  0)
+        )
+        gameOfLife(array)
+
+        println(array.toList().joinToString { it.toList().toString() })
+    }
+
+    fun gameOfLife(board: Array<IntArray>): Unit {
+
+        board.forEachIndexed { i, ints ->
+            ints.forEachIndexed { j, value ->
+                var count = 0
+                if (i - 1 >= 0) {
+                    if (board[i - 1][j] % 10 == 1) {
+                        count++
+                    }
+                    if (j - 1 >= 0 && board[i - 1][j - 1] % 10 == 1) {
+                        count++
+                    }
+                    if (j + 1 <= ints.lastIndex && board[i - 1][j + 1] % 10 == 1) {
+                        count++
+                    }
+                }
+
+                if (i + 1 <= board.lastIndex) {
+                    if (board[i + 1][j] % 10 == 1) {
+                        count++
+                    }
+                    if (j - 1 >= 0 && board[i + 1][j - 1] % 10 == 1) {
+                        count++
+                    }
+                    if (j + 1 <= ints.lastIndex && board[i + 1][j + 1] % 10 == 1) {
+                        count++
+                    }
+                }
+
+                if (j - 1 >= 0 && board[i][j - 1] % 10 == 1) {
+                    count++
+                }
+                if (j + 1 <= ints.lastIndex && board[i][j + 1] % 10 == 1) {
+                    count++
+                }
+
+                if (value == 0) {
+                    if (count == 3) {
+                        board[i][j] += 10
+                    }
+                } else {
+                    if (count == 2 || count == 3) {
+                        board[i][j] += 10
+                    }
+                }
+
+            }
+        }
+
+
+        for (i in 0..board.lastIndex) {
+            for (j in 0..board[0].lastIndex) {
+                board[i][j] /= 10
+            }
+        }
+    }
+}
