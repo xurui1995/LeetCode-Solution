fcfdbba126ece3637bcd149fd1e0dc1d3787b5b6
ruixu <ruixu@tcd.gmail>
2020-12-16 11:14:30 +0000

    add solution3

---
+++ src/3_ LongestSubstringWithoutRepeatingCharacters.kt
@@ -0,0 +1,34 @@
+import org.junit.Test
+import kotlin.math.max
+
+class Solution2 {
+
+    @Test
+    fun test() {
+        var s = "abcabcbb"
+        print(lengthOfLongestSubstring(s))
+        s = "bbbbb"
+        print(lengthOfLongestSubstring(s))
+        s = "pwwkew"
+        print(lengthOfLongestSubstring(s))
+    }
+
+    fun lengthOfLongestSubstring(s: String): Int {
+        val map = hashMapOf<Char, Int>()
+        var ans = 0;
+        var i = 0
+        var j = 0
+
+        while (i < s.length && j < s.length) {
+            if (map.containsKey(s[j])) {
+                val index = map.getOrDefault(s[j], 0)
+                i = max(index + 1, i)
+            }
+
+            map.put(s[j], j)
+            j++
+            ans = max(j - i, ans)
+        }
+        return ans;
+    }
+}
