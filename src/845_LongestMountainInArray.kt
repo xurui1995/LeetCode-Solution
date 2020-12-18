36006c019805e799bc9995da7290e82f65e60849
xurui1995 <ruixutcd@gmail.com>
2020-12-18 23:02:46 +0000

    add solution845

---
+++ src/845_LongestMountainInArray.kt
@@ -0,0 +1,51 @@
+import org.junit.Test
+
+class Solution845 {
+
+    @Test
+    fun test() {
+        println(longestMountain(intArrayOf(2, 1, 4, 7, 3, 2, 5)))
+        println(longestMountain(intArrayOf(2, 2, 2)))
+        println(longestMountain(intArrayOf(875, 884, 239, 731, 723, 685)))
+    }
+
+    fun longestMountain(arr: IntArray): Int {
+
+        if (arr.size < 3) {
+            return 0
+        }
+
+        var ans = 0
+        var temp = 0
+        var isDown = false
+
+        for (i in 1..arr.lastIndex) {
+            if (arr[i] > arr[i - 1]) {
+                if (isDown) {
+                    temp = 2
+                    isDown = false
+                } else {
+                    if (temp == 0) {
+                        temp = 2
+                    } else {
+                        temp++
+                    }
+                }
+
+            } else if (arr[i] < arr[i - 1]) {
+                if (temp != 0) {
+                    isDown = true
+                    temp++
+                    if (temp > ans) {
+                        ans = temp
+                    }
+                }
+            } else {
+                isDown = false
+                temp = 0
+            }
+        }
+
+        return ans
+    }
+}
