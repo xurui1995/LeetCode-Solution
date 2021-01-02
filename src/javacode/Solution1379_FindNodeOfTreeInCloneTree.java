3df0cdbeba7c9bd25f266e54f7e57dfad8fa1770
xurui1995 <ruixutcd@gmail.com>
2021-01-02 11:53:13 +0000

    add Solution1379_FindNodeOfTreeInCloneTree

---
+++ src/javacode/Solution1379_FindNodeOfTreeInCloneTree.java
@@ -0,0 +1,18 @@
+package javacode;
+
+
+public class Solution1379_FindNodeOfTreeInCloneTree {
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if (original == null) {
+            return null;
+        }
+        if (target.equals(original)) {
+            return cloned;
+        }
+        TreeNode left = getTargetCopy(original.left, cloned.left, target);
+        if (left != null) {
+            return left;
+        }
+        return getTargetCopy(original.right, cloned.right, target);
+    }
+}
