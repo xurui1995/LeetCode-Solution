30ffe8541df4ee82e23e30f6974b7328f478c47d
xurui1995 <ruixutcd@gmail.com>
2021-01-08 12:07:21 +0000

    add Solution316_RemoveDuplicateLetters

--- src/javacode/Solution316_RemoveDuplicateLetters.java
+++ src/javacode/Solution316_RemoveDuplicateLetters.java
@@ -2,26 +2,42 @@ package javacode;
 
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class Solution316_RemoveDuplicateLetters {
 
     @Test
     public void test() {
         System.out.println(removeDuplicateLetters("bcabc"));
+        System.out.println(removeDuplicateLetters("cbacdcbc"));
     }
 
     public String removeDuplicateLetters(String s) {
         int[] array = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            array[c - 'a'] = 1;
+            array[s.charAt(i) - 'a']++;
         }
-        StringBuilder ans = new StringBuilder();
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == 1) {
-                char c = (char) ('a' + i);
-                ans.append(c);
+
+        boolean[] visited = new boolean[26];
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (visited[s.charAt(i) - 'a']) {
+                array[s.charAt(i) - 'a']--;
+            } else {
+                while (sb.length() != 0 && array[sb.charAt(sb.length() - 1) - 'a'] > 0 && sb.charAt(sb.length() - 1) > s.charAt(i)) {
+                    visited[sb.charAt(sb.length() - 1) - 'a'] = false;
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+                sb.append(s.charAt(i));
+                visited[s.charAt(i) - 'a'] = true;
+                array[s.charAt(i) - 'a']--;
             }
         }
-        return ans.toString();
+        return sb.toString();
     }
 }
